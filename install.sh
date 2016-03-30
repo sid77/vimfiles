c588e25b20fb0ddbeb002722d0e8b023d76bb127
sid77 <sid77@slackware.it>
2016-03-30 10:48:37 +0100

    Add install script

---
+++ install.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+set -e
+
+GITHUB_DIR="$HOME/src/github.com"
+
+git_clone() {
+  REPO_DIR="$GITHUB_DIR/$1"
+  mkdir -p $REPO_DIR
+  ( cd $REPO_DIR
+    git clone https://github.com/$1/$2.git
+  )
+}
+
+mkdir -p $HOME/.vim/autoload
+git_clone junegunn vim-plug
+( cd $HOME/.vim/autoload
+  ln -sf $GITHUB_DIR/junegunn/vim-plug/plug.vim
+)
+
+mkdir -p $HOME/.vim/colors
+git_clone robertmeta nofrils
+( cd $HOME/.vim/colors
+  ln -sf $GITHUB_DIR/robertmeta/nofrils/colors/nofrils-dark.vim
+)
+
+( cd $HOME
+  ln -sf $GITHUB_DIR/sid77/vimfiles/vimrc .vimrc
+  vim -c 'PlugInstall | quit | quit'
+)
